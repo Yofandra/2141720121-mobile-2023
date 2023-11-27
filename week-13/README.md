928965910202aaa2c4aaad829049cbe39215954f
Yofandra <drayofan@gmail.com>
2023-11-27 23:07:14 +0700

    W13: Jawaban Soal 8

--- week-13/README.md
+++ week-13/README.md
@@ -34,3 +34,13 @@ Penjelasan:
 * Langkah 13 : Menambahkan fungsi addError() yang ketika dipanggil, maka error 'error' akan ditambahkan ke dalam sink dari StreamController
 * Langkah 14 : Menambah bagian Stream, Jadi, jika ada error yang ditambahkan ke dalam Stream, maka lastNumber akan diubah menjadi -1 dan widget akan di-render ulang
 * Langkah 15 : Menambahkan error ke dalam Stream yang bernama numberStream yang sudah ditambahkan tadi
+
+#### Soal 8
+Penjelasan:
+* Langkah 1 : Menambahkan variabel transformer
+* Langkah 2 : Mendefinisikan sebuah StreamTransformer, dimana transformer ini akan mengubah setiap angka di Stream menjadi 10 kali lipat dari nilai aslinya, atau menjadi -1 jika terjadi error. Ketika Stream selesai, sink akan ditutup
+* Langkah 3 : Mengubah data yang mengalir melalui Stream menggunakan transformer, dan kemudian mendengarkan data tersebut. Setiap kali ada data baru, fungsi dalam listen akan dipanggil dengan data baru tersebut sebagai argumen
+
+Output:
+![Alt text](docs/soal8.gif)
+<br>
--- week-13/src/stream_yofandra/lib/main.dart
+++ week-13/src/stream_yofandra/lib/main.dart
@@ -36,6 +36,7 @@ class _StreamHomePageState extends State<StreamHomePage> {
   int lastNumber = 0;
   late StreamController numberStreamController;
   late NumberStream numberStream;
+  late StreamTransformer transformer;
 
   void changeColor() async {
     // await for (var eventColor in colorStream.getColors()) {
@@ -52,10 +53,22 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   @override
   void initState() {
+    super.initState();
+    // colorStream = ColorStream();
+    // changeColor();
+    transformer = StreamTransformer<int, int>.fromHandlers(
+        handleData: (value, sink) {
+          sink.add(value * 10);
+        },
+        handleError: (error, trace, sink) {
+          sink.add(-1);
+        },
+        handleDone: (sink) => sink.close());
+
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
-    stream.listen((event) {
+    stream.transform(transformer).listen((event) {
       setState(() {
         lastNumber = event;
       });
@@ -64,9 +77,6 @@ class _StreamHomePageState extends State<StreamHomePage> {
         lastNumber = -1;
       });
     });
-    super.initState();
-    // colorStream = ColorStream();
-    // changeColor();
   }
 
   @override
@@ -80,7 +90,6 @@ class _StreamHomePageState extends State<StreamHomePage> {
     int myNum = random.nextInt(10);
     numberStream.addNumberToSink(myNum);
     // numberStream.addError();
-
   }
 
   @override
